a11c8686d59a42f5c042a831317f90bc888e5ee9
NizarArhaNaufal <72216735+NizarArhaNaufal@users.noreply.github.com>
2020-10-30 01:50:27 +0700

    Add files via upload

---
+++ Praktikum 1/13.py
@@ -0,0 +1,37 @@
+# program atas
+a = 1
+b = 9
+if(a > 0) and (b > 0):
+    print("Keduanya positif")
+else:
+    print("Keduanya tidak positif")
+
+# program bawah
+a = 1
+b = 9
+if(a > 0):
+    if(b > 0):
+        print("Keduanya Positif")
+    else:
+        print("Keduanya tidak positif")
+else:
+    print("Keduanya tidak positif")
+
+# program atas negatif
+a = -4
+b = -7
+if(a > 0) and (b > 0):
+    print("Keduanya positif")
+else:
+    print("Keduanya tidak positif")
+
+# program bawah negatif
+a = -4
+b = -7
+if(a > 0):
+    if(b > 0):
+        print("Keduanya Positif")
+    else:
+        print("Keduanya tidak positif")
+else:
+    print("Keduanya tidak positif")
