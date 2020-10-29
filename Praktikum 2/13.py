b45510e14316033bfeb2d680f56584c94425c8ad
NizarArhaNaufal <72216735+NizarArhaNaufal@users.noreply.github.com>
2020-10-30 02:31:48 +0700

    Add files via upload

---
+++ Praktikum 2/13.py
@@ -0,0 +1,10 @@
+from random import randint
+angka = 1
+while True:
+    bil = randint(0, 10)
+    print(bil)
+    if bil== 5:
+        break
+    angka += 1
+print("Jumlah Perulangan:",angka)
+    
